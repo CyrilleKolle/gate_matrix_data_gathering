507d44dc4bdee4199f015fa04e4ea86f1e6d3a0c
Tom Victor <vjtomvictor@icloud.com>
2024-02-05 15:33:21 +0100

    refactor binary parser

--- src/movesensor.py
+++ src/movesensor.py
@@ -25,13 +25,9 @@ Usage:
 import sys
 import asyncio
 import csv
-import dataclasses
 import logging
 import signal
-import struct
 from datetime import datetime
-from functools import reduce
-import threading
 
 from bleak import BleakClient
 from bleak import _logger as logger
@@ -42,81 +38,12 @@ from PyQt6.QtCore import QObject, pyqtSignal
 
 from models import Acceleration
 from annotation import AnnotateAccelerometerData
+from src.binary_parser import DataView
 
 SENSOR_ID = "223430000278"
 WRITE_CHARACTERISTIC_UUID = "34800001-7185-4d5d-b431-630e7050e8f0"
 NOTIFY_CHARACTERISTIC_UUID = "34800002-7185-4d5d-b431-630e7050e8f0"
 DATA_POINTS = []
-
-
-class DataView:
-    def __init__(self, array, bytes_per_element=1):
-        """
-        bytes_per_element is the size of each element in bytes.
-        By default, we assume the array is one byte per element.
-        """
-        self.array = array
-        self.bytes_per_element = 1
-
-    def __get_binary(self, start_index, byte_count, signed=False):
-        integers = [self.array[start_index + x] for x in range(byte_count)]
-        bytes = [
-            integer.to_bytes(self.bytes_per_element, byteorder="little", signed=signed)
-            for integer in integers
-        ]
-        return reduce(lambda a, b: a + b, bytes)
-
-    def get_uint_16(self, start_index):
-        bytes_to_read = 2
-        return int.from_bytes(
-            self.__get_binary(start_index, bytes_to_read), byteorder="little"
-        )
-
-    def get_uint_8(self, start_index):
-        bytes_to_read = 1
-        return int.from_bytes(
-            self.__get_binary(start_index, bytes_to_read), byteorder="little"
-        )
-
-    def get_uint_32(self, start_index):
-        bytes_to_read = 4
-        binary = self.__get_binary(start_index, bytes_to_read)
-        return struct.unpack("<I", binary)[0]  # <f for little endian
-
-    def get_float_32(self, start_index):
-        bytes_to_read = 4
-        binary = self.__get_binary(start_index, bytes_to_read)
-        return struct.unpack("<f", binary)[0]  # <f for little endian
-
-    def get_int_32(self, start_index):
-        bytes_to_read = 4
-        binary = self.__get_binary(start_index, bytes_to_read)
-        return struct.unpack("<i", binary)[0]  # <f for little endian
-
-    def get_int_arr(self):
-        byte_length = len(self.array)
-        result = []
-        start_index = 6
-        bytes_to_read = 4
-        number_of_readings = 16
-        for index in [
-            start_index + bytes_to_read * i for i in range(number_of_readings)
-        ]:
-            result.append(self.get_int_32(index))
-        return result
-
-
-# @dataclasses.dataclass
-# class Acceleration:
-#     timestamp: int
-#     timestamp_local: str
-#     ax: float
-#     ay: float
-#     az: float
-#     fall_state: str
-
-#     def as_csv_field(self):
-#         return [self.timestamp, self.timestamp_local, self.ax, self.ay, self.az, self.fall_state]
 
 
 def save_as_csv():
@@ -289,15 +216,3 @@ if __name__ == "__main__":
 
     # threading.Thread(target=run_asyncio_loop, args=(SENSOR_ID,), daemon=True).start()
     # sys.exit(app.exec())
-
-"""
-Application Flow
-
-
-Receive data stream at all times
-  Keep track of the last 10 seconds of data at all times
-  
-Once a button is pressed (fall / no fall)
-  Save data to new file, mark whether it was a fall or not
-
-"""
